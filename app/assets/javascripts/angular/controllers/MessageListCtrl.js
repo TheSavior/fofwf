e5a34de99eb6b2638b94afa6692b45c181b3d1c1
Eli <github@eli-white.com>
2013-08-02 12:41:59 -0700

    Working on messages list

--- app/assets/javascripts/angular/controllers/MessageListCtrl.js
+++ app/assets/javascripts/angular/controllers/MessageListCtrl.js
@@ -1,13 +1,20 @@
-var MessageListCtrl = ['$scope', function($scope) {
-  $scope.phones = [
-    {"id": 1,
-     "name": "Nexus S",
-     "snippet": "Fast just got faster with Nexus S."},
-    {"id": 2,
-     "name": "Motorola XOOM™ with Wi-Fi",
-     "snippet": "The Next, Next Generation tablet."},
-    {"id": 3, 
-     "name": "MOTOROLA XOOM™",
-     "snippet": "The Next, Next Generation tablet."}
-  ];
-}];
+var MessageListCtrl = ['$scope',
+    function($scope) {
+        $scope.messages = [{
+            "time": "3 Days ago",
+            "total": 12,
+            "current": 3,
+            "friends": "Kyle Z, Jay M, Foo B"
+        }, {
+            "time": "1 Hour ago",
+            "total": 3,
+            "current": 2,
+            "friends": "Kyle Z, Bar B"
+        }, {
+            "time": "2 Minutes ago",
+            "total": 1,
+            "current": 0,
+            "friends": ""
+        }, ];
+    }
+];
