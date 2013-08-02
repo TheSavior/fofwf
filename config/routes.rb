f9053784376f33cc7c96c0ffb52a7b348356f52a
Kyle Zemek <kyle.zemek@airbnb.com>
2013-08-02 15:36:03 -0700

    fix stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Fofwf::Application.routes.draw do
   resources :message_threads
-  root "static#index"
+  root "app#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
