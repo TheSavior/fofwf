3d9bf15f371d19cd4c125270d5b02d5aa5c8aa83
Kyle Zemek <kyle.zemek@airbnb.com>
2013-08-03 18:39:31 -0700

    stuff

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -6,20 +6,9 @@ class AppController < ApplicationController
     redirect_to root_url
   end
   def index
-
   end
 
   def show
 
   end
-
-  def me
-    render :text => rest_graph.get('me').inspect
-  end
-
-  private
-
-    def filter_setup_rest_graph
-      rest_graph_setup(:auto_authorize => true)
-    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130802221712) do
+ActiveRecord::Schema.define(version: 20130804012452) do
 
   create_table "message_threads", force: true do |t|
     t.string   "uuid_1"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20130802221712) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "thread_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
