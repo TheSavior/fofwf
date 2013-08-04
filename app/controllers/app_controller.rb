df7f17b591fdf8bdccadd218303719092f4ef237
Kyle Zemek <kyle.zemek@airbnb.com>
2013-08-03 19:36:38 -0700

    blagsd

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -4,18 +4,13 @@ class AppController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     @graph = Koala::Facebook::API.new(user.oauth_token)
-
     profile = @graph.get_object("me")
     friends = @graph.get_connections("me", "friends")
-    puts friends
-    puts friends.size
     redirect_to root_url
   end
   def index
-
-  end
-
-  def show
-
+	if !session[:user_id]
+		render :login, :layout=>"loginLayout"
+	end
   end
 end
