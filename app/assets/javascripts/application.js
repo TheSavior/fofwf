87e3a8d773c284f86e7954ae87d10dee77adb9c7
Eli <github@eli-white.com>
2013-08-03 21:26:16 -0700

    Changing login to be rendered by rails instead of angular

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,12 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-// require jquery
 // require jquery_ujs
 //= require turbolinks
 //= require angular
+//= require angular-bootstrap
+//= require angular-resource
+
 
 //= require app
 //= require_tree ./angular
