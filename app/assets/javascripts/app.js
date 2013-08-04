b9dc1146a1e8dce3b09f165187d81043d3f44a9e
Eli <github@eli-white.com>
2013-08-03 21:28:27 -0700

    Fixing angular routes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,7 @@
 angular.module('fofwfApp', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/login', {templateUrl: 'templates/login.html',   controller: LoginCtrl}).
-      when('/messages', {templateUrl: 'templates/message-list.html',   controller: MessageListCtrl}).
+      when('/', {templateUrl: 'templates/message-list.html',   controller: MessageListCtrl}).
       when('/message/:messageId', {templateUrl: 'templates/message.html', controller: MessageCtrl}).
-      otherwise({redirectTo: '/login'});
+      otherwise({redirectTo: '/'});
 }]);
